6ed330a5d9f362a849f7340f9614cbb14bfe362a
lihaochen <haochenli2000@qq.com>
2017-06-27 13:06:09 +0800

    test

--- app/src/main/java/lhc/highlightview/MainActivity.java
+++ app/src/main/java/lhc/highlightview/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lhc.highlight.HighLight;
+import com.lhc.highlight.position.bottomPosStategy;
 import com.lhc.highlight.position.leftPosStrategy;
 import com.lhc.highlight.shape.RectLightShape;
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         HighLight highLight = new HighLight(this)
-                .addHighLight(R.id.tv_left, R.layout.info_gravity_left_down, new leftPosStrategy(40), new RectLightShape(10, 10))
+                .addHighLight(R.id.tv_left, R.layout.info_gravity_left_down, new bottomPosStategy(40), new RectLightShape(10, 10))
                 .addHighLight(R.id.tv_left_1, R.layout.info_gravity_left_down, new leftPosStrategy(40), new RectLightShape(10, 10))
                 .showInSequence()
                 .show();
